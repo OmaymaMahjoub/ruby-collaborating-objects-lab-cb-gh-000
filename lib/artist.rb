97fa8be1a74933ddd1634de40d6bb166534b0e56
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 15:16:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,45 +1,15 @@
 class Artist
 
-  @@all=Array.new
-
-  def initialize(name)
+  def initialize (name)
     @name=name
-    test=false
-    @@all.each do |s|
-      if s.name==name
-        test=true
-        break
-      end
-    end
-    if test==false
-      @@all<<self
-    end
   end
 
-  attr_accessor name
-
-
-  def self.all
-    @@all
-  end
-
-  def songs
-    arr=Song.all
-    arr.each do |s|
-      if s.artist==self
-        s
-      end
-    end
-  end
-
-  def self.find_or_create_by_name(name)
-    @@all.each do |a|
-      if a.name==name
-        return a
-      end
-    end
+  def name=(name)
     @name=name
-    return self
-
   end
+
+
+    
+
 end
+
