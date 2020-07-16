5ffb6ccd34dd918c54df6d5e6b27e6feeac04eb0
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 15:21:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,27 @@
 class Song
 
   attr_accessor name
+
+  @@all=Array.new
+  
   def initialize (name)
     @name=name
+    @@all<<self
   end
+
+  def name=(name)
+    @name=name
+  end
+
+  def artist=(artist)
+    @artist=artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(name)
+    
 
 end
