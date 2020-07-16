de479715c6d420f8adff06f55b1070b98adc1bdd
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 15:25:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,22 @@ class Artist
     @name=name
   end
 
-
-
+  def self.all
+    arr=Song.all
+    arr2=arr.collect do |s|
+      s.artist.name
+    end
+    arr3=Array.new
+    arr2.each do |a|
+      if arr2.count(a)==1
+        arr3<<a
+      else
+        if arr3.count(a)==0
+          arr3<<a
+        end
+      end
+    end
+    return arr3
+  end
 
 end
