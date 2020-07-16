a5c8dfde457c6711b3dc2937cbb755155b10d053
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 15:12:34 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+
+  @@all=Array.new
+  
+  def initialize(name)
+    @name=name
+    test=false
+    @@all.each do |s|
+      if s.name==name
+        test=true
+        break
+      end
+    end
+    if test==false
+      @@all<<self
+  end
+
+  def name=(name)
+    @name=name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    arr=Song.all
+    arr.each do |s|
+      if s.artist==self
+        s
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.each do |a|
+      if a.name==name
+        return a
+      end
+    end
+    self.name=name
+    return self  
+end
